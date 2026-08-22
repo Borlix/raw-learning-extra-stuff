2c2581ec98bef200df004ca7f2620bdcd17738aa
Borlix <309441135+Borlix@users.noreply.github.com>
2026-08-22 03:40:31 +0100

    Add files via upload

---
+++ NumPy/NumpyPractice.py
@@ -0,0 +1,18 @@
+import numpy as np
+i = np.array([2,4,6,8])
+print(i*2)
+
+y = np.array([10,20,30,40])
+print(np.mean(y))
+
+z = np.array  ([[1,2],
+                [3,4],
+                [5,6]
+                ])
+print(z.shape)
+
+x = np.array([1,2,3,4])
+w = np.array([2,4,6,8])
+y = x @ w
+
+print(y)
